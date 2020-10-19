96ebed46f3d5f63e95a913cce2deeee4d9f61451
rbarbosa1985 <betoanselmo@gmail.com>
2020-10-18 21:35:53 -0300

    first commit

---
+++ pennart/src/main/java/com/pencet/pennart/PennartApplication.java
@@ -0,0 +1,13 @@
+package com.pencet.pennart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PennartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PennartApplication.class, args);
+	}
+
+}
